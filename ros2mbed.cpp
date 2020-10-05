7692410af929047535ec0ede4102f789c45331e9
Kotakku <kotakkucu@gmail.com>
2020-10-05 18:10:08 +0900

    Fix: for baremetal profile

--- ros2mbed.cpp
+++ ros2mbed.cpp
@@ -1,6 +1,8 @@
 #include "ros2mbed.h"
 
+#if MBED_CONF_NSAPI_PRESENT
 bool ros2::init(NetworkInterface* comm_instance, const char* p_server_ip, uint16_t server_port, bool is_tcp)
 {
   return ros2::init((void*)comm_instance, p_server_ip, server_port, is_tcp);
 }
+#endif //MBED_CONF_NSAPI_PRESENT
